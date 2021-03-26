679a931ddb9d4a1e5ea118c66846384c89b72113
Chetan Aggarwal <Chetan.Aggarwal@amdocs.com>
2021-03-27 00:01:37 +0530

    StopWatch done

--- src/components/SWApp.js
+++ src/components/SWApp.js
@@ -3,11 +3,69 @@ import React from 'react';
 class SWApp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {hr : '00', min : '00', sec : '00'};
+        this.state = {hr : 0, min : 0, sec : 0, setIntervalId : '', action : 'Start'};
     }
 
     homePage = () => {
         window.location = '/';
+    }
+
+    handleIncrement = () => {
+        let data =  this.state.sec;
+        ++ data;
+
+        if(data === 60){
+            let minData = this.state.min;
+            ++ minData;
+            if(minData === 60){
+                let hrData = this.state.hr;
+                ++ hrData;
+                if(hrData === 24){
+                    this.setState({sec : 0, min : 0, hr : 0});
+                }else{
+                    this.setState({sec : 0, min : 0, hr : hrData});
+                }
+            }else{
+                this.setState({sec : 0, min : minData});
+            }
+        }else{
+            this.setState({sec : data})
+        }
+      
+    }
+
+    handleSetInterval = () => {
+        if(this.state.action === 'Start'){
+            let id = setInterval(this.handleIncrement, 1000);
+            this.setState({setIntervalId : id, action : 'Stop'});
+        }else{
+            clearInterval(this.state.setIntervalId);
+            this.setState({action : 'Start'});
+        }
+    }
+
+    handleClearInterval = () => {
+        clearInterval(this.state.setIntervalId);
+        this.setState({hr : 0, min : 0, sec : 0, action : 'Start'})
+    }
+
+    handleDisplay = () => {
+        let hrAns = this.state.hr;
+        if(this.state.hr < 10){
+            hrAns = '0' + hrAns;
+        }
+
+        let minAns = this.state.min;
+        if(this.state.min < 10){
+            minAns = '0' + minAns;
+        }
+
+        let secAns = this.state.sec;
+        if(this.state.sec < 10){
+            secAns = '0' + secAns;
+        }
+
+        return hrAns  + ":" +minAns + ":" + secAns;
     }
 
     render() {
@@ -18,10 +76,10 @@ class SWApp extends React.Component {
                     <div className="utility-header">Utility Apps</div>
                 </header>
                 <div className="stopwatch-container">
-                    <div className="stopwatch">{this.state.hr + ":" + this.state.min + ":" + this.state.sec}</div>
+                    <div className="stopwatch">{this.handleDisplay()}</div>
                     <div className="stopwatch-button-container">
-                        <button>Start</button>
-                        <button>Reset</button>
+                        <button onClick={this.handleSetInterval}>{this.state.action}</button>
+                        <button onClick={this.handleClearInterval}>Reset</button>
                     </div>
                     
 
